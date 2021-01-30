0f03a4b36a37ce198e46e4073c09cf1350677918
JoeFulton2 <JoeFulton2@urmomgay.xyz>
2021-01-30 11:57:21 +0000

    Base

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include"sensor.hpp"
+#include"sensor.cpp"
 #include<iostream>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
+
+  int counter; 
+    printf("Program Name Is: %s",argv[0]); 
+    if(argc==1) 
+        printf("\nNo Extra Command Line Argument Passed Other Than Program Name"); 
+    if(argc>=2) 
+    { 
+        printf("\nNumber Of Arguments Passed: %d",argc); 
+        printf("\n----Following Are The Command Line Arguments Passed----"); 
+        for(counter=0;counter<argc;counter++) 
+            printf("\nargv[%d]: %s",counter,argv[counter]); 
+    } 
+
 	if (argc < 2) {
-        	cerr << "Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << std::endl;
+        	cerr << "n\Usage: " << argv[0] << " <sampleTime> <precisionBits> <numberOfSimulatedSamples>" << std::endl;
     	}
 	else{
 		cout << "You passed the following arguments: ";
